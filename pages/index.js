4a482842531ede8f2b31de7bf4b96cf467e9b747
Matt Galang <magalang1@gmail.com>
2021-05-10 15:08:08 -0700

    Add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import styles from '../styles/Home.module.css'
-
 import { fetchEntries } from '../util/contentfulProjects'
 import Project from '../components/Project'
 
 export default function Home({projects}) {
   return (
-    <div className={styles.container}>
+    <div className="container">
         {projects.map(project => (
           <Project key={project.date} {...project}/>
         ))}
@@ -16,7 +14,6 @@ export default function Home({projects}) {
 export async function getStaticProps() {
   const res = await fetchEntries()
   const projects = await res.map(p => p.fields)
-  console.log(projects)
   return {
     props: {
       projects
